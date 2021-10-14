978d4f0a101c9383f0f937383af355927927a422
Robin de Witte <r.dewitte@student.fontys.nl>
2021-10-14 15:49:51 +0200

    cqrs werkend

--- fictivusforum_topicservice/repositories/ResponseContext.cs
+++ fictivusforum_topicservice/repositories/ResponseContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using fictivusforum_topicservice.DataModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace fictivusforum_topicservice.repositories
@@ -13,7 +14,7 @@ namespace fictivusforum_topicservice.repositories
         {
         }
 
-        public DbSet<> Posts { get; set; }
+        public DbSet<Response> Posts { get; set; }
 
     }
 }
